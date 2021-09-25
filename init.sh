bed76b15ac04eebb2604719f4df0f8910c2a9752
gattyan27 <radio.radirer@gmail.com>
2021-09-25 21:06:41 +0900

    ddddd

--- init.sh
+++ init.sh
@@ -50,7 +50,7 @@ done
 # done
 mkdir /home/$username
 mkfs -t ext4 $device
-mount $device /home/$username
+mount $device /
 # sudo apt install -y nfs-common
 # sudo apt install -y cifs-utils
 # sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $volume_id.efs.ap-northeast-1.amazonaws.com:/ /home/$username
